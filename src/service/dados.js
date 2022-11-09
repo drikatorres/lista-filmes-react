dd6e559f4d1074311ee310c82d4aba11f66fc2a5
drikatorres <torrescruz.drika@gmail.com>
2022-11-08 21:18:57 -0300

    first commit

---
+++ src/service/dados.js
@@ -0,0 +1,28 @@
+const filmes = [
+    {
+        id: 1,
+        nome: "Manuelita",
+        genero: "Animação",
+        imagem: "https://br.web.img3.acsta.net/pictures/210/608/21060831_20131125202729768.jpg"
+    },
+    {
+        id: 2,
+        nome: "Especial 50 anos Doctor Who",
+        genero: "Ficção Científica",
+        imagem: "http://doctorwhobrasil.com.br/wp-content/uploads/2013/09/1237199_10202065437424492_1571387061_o.jpg"
+    },
+    {
+        id: 3,
+        nome: "Cuba e o cameraman",
+        genero: "Documentário",
+        imagem: "https://catracalivre.com.br/wp-content/uploads/2019/07/cartaz-cuba-e-o-cameraman.jpg"
+    },
+    {
+        id: 4,
+        nome: "Black is king",
+        genero: "Filme Musical",
+        imagem: "https://static.wixstatic.com/media/c381b2_d2a4fc8475664662b5129485be56ecae~mv2.jpeg/v1/fit/w_1000%2Ch_675%2Cal_c%2Cq_80/file.jpeg"
+    }
+]
+
+export default filmes
